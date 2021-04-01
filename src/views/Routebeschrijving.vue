d4f52c1a70bbc344bd2d081ac1db5fbfbbdc3e37
ndr <andrekolmeijer@gmail.com>
2021-04-01 19:52:19 +0200

    Choice: public, cleanup: webpack

--- src/views/Routebeschrijving.vue
+++ src/views/Routebeschrijving.vue
@@ -104,11 +104,8 @@ export default {
     name: 'Routebeschrijving',
     components: { ArrowIcon, DirectionsIcon, DownloadIcon },
     setup() {
-        // const routebeschrijving = require("@/assets/routebeschrijving.pdf")
-        // const routebeschrijvingPrint = require("@/assets/routebeschrijving-print.pdf")
         const publicPath = process.env.BASE_URL
 
-        // return { routebeschrijving, routebeschrijvingPrint }
         return { publicPath }
     }
 }
